e90660dc2369c39c31cc557d0393103d43d4ccc3
mariel-rs <mariel.rsal@gmail.com>
2022-05-15 00:07:29 -0500

    Finish example3

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,9 @@
+/*
+  node main.js
+*/
+
+const logger1 = require('./logger_1')
+const logger2 = require('./logger_2')
+
+logger1('Mensaje desde logger1')
+logger2.verbose('Mensaje usando propiedad verbose de logger2')
