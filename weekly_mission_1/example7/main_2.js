a191be0c32fbac073a0477cc3e9bd9e0d3e66ee9
mariel-rs <mariel.rsal@gmail.com>
2022-05-20 22:27:25 -0500

    Finished example7 - Create a small extra function in logger.js - Imported more items from logger.js to  main_2.js

---
+++ weekly_mission_1/example7/main_2.js
@@ -0,0 +1,7 @@
+// This import will import only certain parts of Logger.js
+
+import { LEVELS, DEFAULT_LEVEL, hello } from './logger.js'
+
+console.log(LEVELS, DEFAULT_LEVEL)
+hello()
+// log('Hello world')
