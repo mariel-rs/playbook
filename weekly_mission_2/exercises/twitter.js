dda8fd393d8a810a0df20792c4d0f3fe2954b0f3
mariel-rs <mariel.rsal@gmail.com>
2022-05-28 20:31:28 -0500

    Continue exercise 1: - Twitter exercise

---
+++ weekly_mission_2/exercises/twitter.js
@@ -0,0 +1,19 @@
+// Twitter exercise
+
+const user = {
+    username: "zerostaticio",
+    bio: "We make make incredible #Jamstack themes for @GoHugoIO ...",
+    location: "Brisbane, Queensland"
+}
+
+const trending_topic = {
+    name: "#GiftAGamer",
+    url: "http://twitter.com/search?q=%23GiftAGamer",
+    query: "%23GiftAGamer",
+    tweet_volume: null,
+    date: new Date()
+}
+
+const hashtag = {
+    text: "nodejs"
+}
